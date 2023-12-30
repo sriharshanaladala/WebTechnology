a6ba82c9008fe33a863998dd5fb9868bdcc30bbb
sriharsha <sriharshanaladala999@gmail.com>
2023-12-30 09:30:38 +0530

    java script

---
+++ javascript/datatypes/datatypes.js
@@ -0,0 +1,23 @@
+const a="hello";
+console.log(typeof a)
+const b=100;
+console.log(typeof b)
+const c=true;
+console.log(typeof c);
+const d=null;
+console.log(typeof d);
+let e;
+console.log(typeof e); 
+const f=25;
+console.log(typeof (typeof f))
+const g=true+false
+console.log(typeof g)
+console.log(typeof 100)
+console.log(typeof "sri")
+console.log(typeof true)
+console.log(typeof (true+true))
+console.log(typeof true+false)
+let h=100n;
+console.log(typeof h)
+
+
---
+++ javascript/variables/variables.js
@@ -0,0 +1,25 @@
+//variables
+//var key word
+var a;            //declaration
+a=10;            //INITIALIZ
+console.log(a);    //UTILIZE
+var a;              //re declare
+a=20;             //re init
+console.log(a);     //re utilize
+
+var a = 30;
+console.log(a);      //re declare and initialize in same line
+
+let b;                       //declaration
+b="helo";                 //INITIALIZ
+console.log(b);             //UTILIZE
+//let b                     //re declare is not possible
+b="world"                 //re init
+console.log(b);             //re utilize
+//let b = "helloworld";  //re declare and initialize in same line is not possible
+
+// const c;
+// c = "hi";
+// console.log(c)
+const c=10              //re declare and initialize in same line is only possible
+console.log(c)

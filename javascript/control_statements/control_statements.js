bed620bd34e6b62237a3521e11cc15eb790026cf
sriharsha <sriharshanaladala999@gmail.com>
2024-01-05 23:51:27 +0530

    ggg

--- javascript/control_statements/control_statements.js
+++ javascript/control_statements/control_statements.js
@@ -74,12 +74,68 @@ switch(color)
 }
 console.log("end")
 
-
-let p = 12;
-if (p%1==0 && p%p==0 )
+// ==================================================
+//prime numbers
+let p =100;
+if (p<=1)
 {
-    console.log("it is a prime");
+    console.log(p+" is not prime");
+}
+else if ( p%2 && p%3)
+{
+    console.log(p+" is  a prime");
+}
+else if(p/2>=2)
+{
+ console.log(p+" is not a prime ");
 }
 else{
-    console.log("it is not a prime")
+    console.log(p+" is a prime");
 }
+
+// ==================================================
+//eligible for army or not
+let age = 19;
+let height = 175;
+let weight = 70;
+if (age<18)
+{
+    console.log("due to less age"+age+" years you are not aligible for army")
+}
+else if(height < 170)
+{
+    console.log("due to insufficiant height"+height+" cm you are not aligible for army")
+}
+else if(weight<60)
+{
+    console.log("due to insufficiant weight"+weight+" kg's you are not aligible for army")
+}
+else
+{
+    console.log("you are eligible for army")
+}
+
+// ==================================================
+//divisible by 5 &7 or not
+
+let num1 =35;
+if (num1%5==0 && num1%7==0)
+{
+    console.log(num1+" is divisible by 5 and 7")
+}
+else {
+    console.log(num1+" not divisible by 5 and 7")
+}
+
+// ==================================================
+// program to check the data type
+let num2="sri";
+if (typeof(num2)===typeof(1))
+{
+    console.log(num2+" is a number type")
+}
+else if(typeof(num2)===typeof("string"))
+{
+    console.log(num2+" is a string type")
+}
+else if()

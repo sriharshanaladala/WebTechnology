3179cc9d56d222ea5ce0663f65563fdd0882e401
sriharsha <sriharshanaladala999@gmail.com>
2024-01-08 10:39:09 +0530

    java script functions

---
+++ javascript/functions/functons.js
@@ -0,0 +1,24 @@
+function sum(a,b)
+{
+    console.log(a+b)
+}
+
+sum(1,2)
+
+sum(1)
+sum(1,2,3,4)
+function print(a,b)
+{
+    console.log(a+b);
+    return a+b
+}
+print("hello", "world")
+print(1,"2")
+print(1, "-2")
+print(5-5,100*100)
+print(print(5+5),30);
+print(true, false)
+let c= print(100, 625)
+ print(100,c)
+
+function r7

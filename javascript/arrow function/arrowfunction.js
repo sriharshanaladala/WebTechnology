702fc333198862b997d5af95a156455f8908e614
sriharsha <sriharshanaladala999@gmail.com>
2024-01-10 09:52:06 +0530

    hoisting in functions

--- javascript/arrow function/arrowfunction.js
+++ javascript/arrow function/arrowfunction.js
@@ -16,7 +16,22 @@ let b = arrfun3(5,6)
 console.log(b)
 // -------------------------------------------
 
-let arrfun4 = 
+// hoisting in functions
 
+demo()
+
+function demo(){
+    console.log("helloworld");
+}
+
+console.log(h)
+let num1= () => console.log("helloworld")
+var h = num1();
+
+
+// num2()
+let num2 =function () {
+    console.log("hellow world")
+}
 
 

c30cc2508100accdd0f067df3ec1916d3e66d102
sriharsha <sriharshanaladala999@gmail.com>
2024-02-06 14:07:47 +0530

    api fetchig

--- javascript/lopps/loops.js
+++ javascript/lopps/loops.js
@@ -29,3 +29,34 @@ do
     i++
 }while(i>=100);
 
+//for in
+
+// for in loop its is  used to iterate throught the array and object 
+//it gives arrauy index positions and object key 
+let arr = [10,20,30,40]
+for(let x in arr)
+{
+    console.log(x);
+    console.log(arr[x]);
+}
+
+let obj = {
+    name:'abc',
+    sal:1000,
+    id : 101,
+    role:'developer'
+}
+
+for(let x in obj){
+    console.log(x);
+    console.log(obj[x]);
+}
+
+//for of : it gives the array element.
+for(let x of arr){
+    console.log(x);
+}
+for(let x of obj)
+{
+    console.log(x);
+}
